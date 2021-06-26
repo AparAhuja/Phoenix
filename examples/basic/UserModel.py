7b17b3e22afaec2b997acee2a86d414e21a8f80a
Apar Ahuja <66245948+AparAhuja@users.noreply.github.com>
2021-06-26 20:23:08 +0530

    Update UserModel.py

--- examples/basic/UserModel.py
+++ examples/basic/UserModel.py
@@ -19,6 +19,6 @@ class UserModel(Model.ScheduledModel):
 	def __init__(self):
 		super(UserModel, self).__init__()
 		self.resource_production_fn = resource_production_fn
-		#self.insert_state(name, resource_consumption_fn, division_age, min_atp_req_to_divide, no_of_div_before_death)
+		#self.insert_state(name, resource_consumption_fn, atp_production_fn, division_age, min_atp_req_to_divide, no_of_div_before_death)
 		self.insert_state('A', resource_consumption_fnA, atp_production_fnA, (2, 0), (4, 0), (4, 0))
 		self.insert_state('B', resource_consumption_fnB, atp_production_fnB, (2, 0), (2, 0), (4, 0))
